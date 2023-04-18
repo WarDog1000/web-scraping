94eed0c89dc83f943ebff342558c016905262579
WarDog1000 <jhonatansouzameza100@gmail.com>
2023-04-18 20:26:21 -0300

    add  DOM traversing with cheerio

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Node.js Web Scraping Example
+# Node.js Web Scraping Example
 
 Este código es un ejemplo de cómo se puede utilizar Node.js para hacer una solicitud HTTP a un sitio web y analizar el contenido HTML de la respuesta. El código requiere dos bibliotecas de Node.js: `cheerio` y `request-promise`. `cheerio` es una biblioteca que se utiliza para analizar y manipular el contenido HTML, similar a la forma en que se utiliza jQuery en el navegador. `request-promise` es una biblioteca que se utiliza para hacer solicitudes HTTP a sitios web.
 
@@ -7,3 +7,34 @@ En el código, se define una función `init()` que es asíncrona, lo que signifi
 El código está comentado en la línea 5, donde se muestra la estructura del HTML de la página web que se está raspando. Sin embargo, esta línea está comentada y no se ejecutará a menos que se quite el comentario.
 
 En resumen, este código utiliza `cheerio` y `request-promise` para hacer una solicitud HTTP a un sitio web y analizar el contenido HTML de la respuesta.
+
+## Iniciar proyecto en Node.js
+- create new terminal | ctrl + shift + 5
+- `npm init -y` crea un archivo llamado `package.json` que describe nuestro script:
+  - Cual es su script principal
+  - Como podemos arrancarlo
+  - Lista sus dependencias
+
+## Instalar las dependencias
+- `npm install cheerio`
+- `npm install request`
+- `npm install request-promise`
+- `npm i fs-extra`
+
+O en su defecto `npm i cheerio request request-promise fs-extra`
+
+##
+***Cheerio*** (https://cheerio.js.org/docs/basics/loading): Permite escanear un sitio web
+  
+***Request npm*** (https://www.npmjs.com/package/request): Permite hacer peticiones http y traer la información de la página. Utiliza Async/Await.
+  
+***Request-promise*** (https://www.npmjs.com/package/request): Permite manejar peticiones http y manejar las respuestas utilizando funciones asincronas.
+  
+***fs-extra*** (File System Extra) (https://www.npmjs.com/package/fs-extra): Permite crear y guardar documentos utilizando Async/Await.
+
+(https://toscrape.com/): Posee sitios web que puedes analizar de forma cencilla.
+
+## Ejecutar el script
+ `node index.js`
+
+
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // bibliotecas necesarias para poder alanilzar el sitio web
 const cheerio = require('cheerio'),
-      request = require('request-promise')
+      request = require('request-promise'),
+      fs = require('fs-extra')
+
+// ============ CREACION DE UN ARCHIVO CON LA INFORMACION RECOLECTADA ============
+const writeStream = fs.createWriteStream('quotes.csv') // crea el archivo y le da un titulo
+
 
 // async function init() {
 //   // hace una peticion al sitio http://quotes.toscrape.com/
@@ -23,29 +28,60 @@ async function init() {
   // ============ DEBUG POR CONSOLA ============
   // titulo de la pagina
   const websiteTitle = $('title').html()
-  console.log(websiteTitle)
+  // console.log(websiteTitle)
 
   // <head> de la pagina
   const websiteDescription = $('head').html()
-  console.log(websiteDescription)
+  // console.log(websiteDescription)
 
   // texto del <h1> de la pagina y limpiando los espacios en blanco que no se utilizan
   const websiteTitleH1 = $('h1').text().trim()
-  console.log(websiteTitleH1)
+  // console.log(websiteTitleH1)
 
   // selecciona todo el primer elemento .quote que encuentre
   const quote = $('.quote').html()
-  console.log(quote)
+  // console.log(quote)
 
   // selecciona todo el primer elemento <a> que encuentre dentro .quote
   const quoteAnchor= $('.quote').find('a').html()
-  console.log(quoteAnchor)
-
+  // console.log(quoteAnchor)
+  
   // selecciona todo el tercer elemento .quote
   const thirdQuote= $('.quote').next().next().html()
-  console.log(thirdQuote)
+  // console.log(thirdQuote)
 
+  // selecciona el col-md-8 con todas las quote qwe tiene dentro
+  const parentContainerClass = $('.row .col-md-8').parent().next().html()
+  // console.log(containerClass)
+  
+  // selecciona el primer elemento hijo de  col-md-8
+  const childrenContainerClass = $('.row .col-md-8').children().html()
+  // console.log(childrenContainerClass)
+
+  // selecciona todos los div con la clase quote en su texto y los recorre con la funcion .each
+  const quoteSpantext = $('.quote span.text').each( (index, element) => {
+    // console.log(index, $(element).html())
+    // console.log(index, $(element).text())
+
+    const elementText = $(element).text()
+    const onlyElemetText = elementText.replace(/(^\“|\”$)/g, '') // remplaza las comillas por nada
+    // console.log(index, onlyElemetText)
+  })
+  
+  // ============ CREACION DE UN ARCHIVO CON LA INFORMACION RECOLECTADA ============
+  writeStream.write('Quote|Author|Tags\n')
+  
+  // recorre los elementos con la clase .quote
+  $('.quote').each( (index, element) => {
+    const text = $(element).find('span.text').text().replace(/(^\“|\”$)/g, '')
+    const autor = $(element).find('span small.author').text()
+    const tags = []
+    $(element).find('.tags a.tag').each( (i, el) => tags.push($(el).text()))
+    // console.log(tags)
+    
+    // ============ CREACION DE UN ARCHIVO CON LA INFORMACION RECOLECTADA ============
+    writeStream.write(`${text}|${autor}|${tags}\n`) // \n permite hacer un salto de linea cuando vuelva a recorrer y de esa manera crear otra fila
+  })
 
 }
-
 init()
